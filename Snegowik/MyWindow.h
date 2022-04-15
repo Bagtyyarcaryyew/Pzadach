0ce6ad16928ae48ce5d10d69a82a3a1e52d06da3
Bagtyyarcaryyew <103555487+bagtyy@users.noreply.github.com>
2022-04-15 16:05:37 +0500

    Initial commit

---
+++ Snegowik/MyWindow.h
@@ -0,0 +1,31 @@
+#ifndef MYWINDOW_H_
+#define MYWINDOW_H_
+
+#include "Window.h"
+
+class MyWindow: public Window
+{
+public:
+	static constexpr int WINDOW_WIDTH  = 1024;
+	static constexpr int WINDOW_HEIGHT = 720;
+
+	virtual void update() override;
+	virtual void render() override;
+	virtual void setupGL() override;
+
+protected:
+	double seconds { 0.0 };
+	double hours   { 0.0 };
+	double minutes { 0.0 };
+
+	void draw_clock();
+
+public:
+	MyWindow();
+	virtual ~MyWindow() = default;
+
+
+
+};
+
+#endif /* MYWINDOW_H_ */
